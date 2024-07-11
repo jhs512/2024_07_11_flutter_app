82aeab007cb9b569eb45106adf371b91ce10f7eb
jhs512 <jangka512@gmail.com>
2024-07-11 12:09:18 +0900

    Column, static value, dynamic value, ElevatedButton

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,7 +17,31 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const Placeholder(),
+      home: const HomePage(),
     );
+  }
+}
+
+class HomePage extends HookConsumerWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    const count1 = 100;
+    final count2 = useState(200);
+
+    return Scaffold(
+        body: Column(
+      children: [
+        const Text('static count : $count1'),
+        Text('dynamic count : ${count2.value}'),
+        ElevatedButton(
+          onPressed: () {
+            count2.value++;
+          },
+          child: const Text('Increment'),
+        ),
+      ],
+    ));
   }
 }
